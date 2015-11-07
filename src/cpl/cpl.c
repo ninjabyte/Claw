0ad950b1b66301a91b160046e3ef87ea6c32136f
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-07 16:45:32 +0100

    Started with compiler

---
+++ src/cpl/cpl.c
@@ -0,0 +1,8 @@
+/*
+ * cpl.c
+ *
+ *  Created on: 07.11.2015
+ *      Author: Benedikt
+ */
+
+
---
+++ src/cpl/cpl.h
@@ -0,0 +1,13 @@
+/*
+ * cpl.h
+ *
+ *  Created on: 07.11.2015
+ *      Author: Benedikt
+ */
+
+#ifndef CPL_CPL_H_
+#define CPL_CPL_H_
+
+
+
+#endif /* CPL_CPL_H_ */
