8ef85025276e8a3e2b38fefe8774ce8559740bb5
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-07 19:33:47 +0100

    Fixed some errors

--- src/lex/lex.c
+++ src/lex/lex.c
@@ -66,95 +66,94 @@ int lex_next(LexState* ls, KeywordInfo *kf)
 			}
 		}
 
-		switch(c)
-		{
-		case 0:
-		case EOF:
-			RETURN_TOKEN(TK_EOI);
-		case '\n':	// line breaks
-		case '\r':
-			ls->line++;
-			RETURN_TOKEN(TK_NEWLINE);
-		case ' ': case '\f': case '\t': case '\v': case ';': // whitespace
-			RETURN_TOKEN(TK_WHITESPACE);
-		case '+': RETURN_TOKEN(TK_PLUS);
-		case '-':	// TK_MINUS or TK_COMMENT
-			REQUIRE_NEXT_TOKEN();
-		case '*': RETURN_TOKEN(TK_MULTIPLY);
-		case '/': RETURN_TOKEN(TK_DIVIDE);
-		case '%': RETURN_TOKEN(TK_MODULUS);
-		case '<':	// TK_LESS, TK_LESSEQUALS or TK_BIT_SL
-			REQUIRE_NEXT_CHAR();
-		case '>':	// TK_GREATER, TK_GREATEREQUALS or TK_BIT_SR
-			c1 = fgetc(ls->src);
-			switch(c1)
-			{
-			case '>': RETURN_TOKEN(TK_BIT_SR);
-			case '=': RETURN_TOKEN(TK_GREATEREQUALS);
-			}
-			ungetc(c1, ls->src);
-			RETURN_TOKEN(TK_GREATER);
-		case '=':	// TK_ASSIGN or TK_EQUALS
-			c1 = fgetc(ls->src);
-			if (c1 == '=')
-				RETURN_TOKEN(TK_EQUALS);
-			ungetc(c1, ls->src);
-			RETURN_TOKEN(TK_ASSIGN);
-		case '!':	// TK_BIT_NOT or TK_UNEQUALS
-			c1 = fgetc(ls->src);
-			if (c1 == '=')
-				RETURN_TOKEN(TK_UNEQUALS);
-			ungetc(c1, ls->src);
-			RETURN_TOKEN(TK_BIT_NOT);
-		case '&':	// TK_AND or TK_BIT_AND
-			c1 = fgetc(ls->src);
-			if (c1 == '&')
-				RETURN_TOKEN(TK_AND);
-			ungetc(c1, ls->src);
-			RETURN_TOKEN(TK_BIT_AND);
-		case '|':	// TK_OR or TK_BIT_OR
-			c1 = fgetc(ls->src);
-			if (c1 == '|')
-				RETURN_TOKEN(TK_OR);
-			ungetc(c1, ls->src);
-			RETURN_TOKEN(TK_BIT_OR);
-		case '^': RETURN_TOKEN(TK_BIT_XOR);
-		case ',': RETURN_TOKEN(TK_COMMA);
-		case '(': RETURN_TOKEN(TK_BR_OPEN);
-		case ')': RETURN_TOKEN(TK_BR_CLOSE);
-		case '{': RETURN_TOKEN(TK_CBR_OPEN);
-		case '}': RETURN_TOKEN(TK_CBR_CLOSE);
-		case '[': RETURN_TOKEN(TK_BBR_OPEN);
-		case ']': RETURN_TOKEN(TK_BBR_CLOSE);
-		case '0': case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
-			REQUIRE_NEXT_CHAR(c);
-		case '"':
-			RETURN_TOKEN(TK_QUOTE);
-		default:
-			{
-				char name[17];
-				int i;
-				for (i=0; i<16; i++)
+		switch(c) {
+			case 0:
+			case EOF:
+				RETURN_TOKEN(TK_EOI);
+			case '\n':	// line breaks
+			case '\r':
+				ls->line++;
+				RETURN_TOKEN(TK_NEWLINE);
+			case ' ': case '\f': case '\t': case '\v': case ';': // whitespace
+				RETURN_TOKEN(TK_WHITESPACE);
+			case '+': RETURN_TOKEN(TK_PLUS);
+			case '-':	// TK_MINUS or TK_COMMENT
+				REQUIRE_NEXT_CHAR();
+			case '*': RETURN_TOKEN(TK_MULTIPLY);
+			case '/': RETURN_TOKEN(TK_DIVIDE);
+			case '%': RETURN_TOKEN(TK_MODULUS);
+			case '<':	// TK_LESS, TK_LESSEQUALS or TK_BIT_SL
+				REQUIRE_NEXT_CHAR();
+			case '>':	// TK_GREATER, TK_GREATEREQUALS or TK_BIT_SR
+				c1 = fgetc(ls->src);
+				switch(c1)
 				{
-					name[i] = (char) c;
-					c = fgetc(ls->src);
-					if (c == EOF)
-						break;
-					if (!isLetter(c))
-					{
-						ungetc(c, ls->src);
-						break;
-					}
+				case '>': RETURN_TOKEN(TK_BIT_SR);
+				case '=': RETURN_TOKEN(TK_GREATEREQUALS);
 				}
-				name[++i] = 0;
-				int j;
-				for (j=TOK_FIRST_KW; j<=TOK_LAST_KW; j++)
-					if (strlen(lex_keywords[j-TOK_FIRST_KW]) == i && strncmp(name, lex_keywords[j-TOK_FIRST_KW], i) == 0)
-						RETURN_TOKEN(j);
+				ungetc(c1, ls->src);
+				RETURN_TOKEN(TK_GREATER);
+			case '=':	// TK_ASSIGN or TK_EQUALS
+				c1 = fgetc(ls->src);
+				if (c1 == '=')
+					RETURN_TOKEN(TK_EQUALS);
+				ungetc(c1, ls->src);
+				RETURN_TOKEN(TK_ASSIGN);
+			case '!':	// TK_BIT_NOT or TK_UNEQUALS
+				c1 = fgetc(ls->src);
+				if (c1 == '=')
+					RETURN_TOKEN(TK_UNEQUALS);
+				ungetc(c1, ls->src);
+				RETURN_TOKEN(TK_BIT_NOT);
+			case '&':	// TK_AND or TK_BIT_AND
+				c1 = fgetc(ls->src);
+				if (c1 == '&')
+					RETURN_TOKEN(TK_AND);
+				ungetc(c1, ls->src);
+				RETURN_TOKEN(TK_BIT_AND);
+			case '|':	// TK_OR or TK_BIT_OR
+				c1 = fgetc(ls->src);
+				if (c1 == '|')
+					RETURN_TOKEN(TK_OR);
+				ungetc(c1, ls->src);
+				RETURN_TOKEN(TK_BIT_OR);
+			case '^': RETURN_TOKEN(TK_BIT_XOR);
+			case ',': RETURN_TOKEN(TK_COMMA);
+			case '(': RETURN_TOKEN(TK_BR_OPEN);
+			case ')': RETURN_TOKEN(TK_BR_CLOSE);
+			case '{': RETURN_TOKEN(TK_CBR_OPEN);
+			case '}': RETURN_TOKEN(TK_CBR_CLOSE);
+			case '[': RETURN_TOKEN(TK_BBR_OPEN);
+			case ']': RETURN_TOKEN(TK_BBR_CLOSE);
+			case '0': case '1': case '2': case '3': case '4':
+			case '5': case '6': case '7': case '8': case '9':
+				REQUIRE_NEXT_CHAR();
+			case '"':
+				RETURN_TOKEN(TK_QUOTE);
+			default:
+				{
+					char name[17];
+					int i;
+					for (i=0; i<16; i++)
+					{
+						name[i] = (char) c;
+						c = fgetc(ls->src);
+						if (c == EOF)
+							break;
+						if (!isLetter(c))
+						{
+							ungetc(c, ls->src);
+							break;
+						}
+					}
+					name[++i] = 0;
+					int j;
+					for (j=TOK_FIRST_KW; j<=TOK_LAST_KW; j++)
+						if (strlen(lex_keywords[j-TOK_FIRST_KW]) == i && strncmp(name, lex_keywords[j-TOK_FIRST_KW], i) == 0)
+							RETURN_TOKEN(j);
 
-				memcpy(kf->name, name, i+1);
-				RETURN_TOKEN(TK_NAME);
+					memcpy(kf->name, name, i+1);
+					RETURN_TOKEN(TK_NAME);
 			}
 		}
 	}
