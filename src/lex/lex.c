798111c7e75ba4a3f7f66e4cf0366329838671e1
RobinDeWalvis <voetterrb1a@gmail.com>
2015-11-08 02:05:39 +0100

    fixerino

--- src/lex/lex.c
+++ src/lex/lex.c
@@ -140,15 +140,9 @@ int lex_nextWord(LexState* ls, char c0)
 		return TK_NONE;
 	}
 
-<<<<<<< HEAD
-	name[0] = cx;
+	ls->kf.name[0] = cx;
 	uint8_t i;
 	for (i=1 ; i<16; i++)
-=======
-	ls->kf.name[0] = cx;
-	uint8_t i = 1;
-	for ( ; i<16; i++)
->>>>>>> origin/master
 	{
 		cx = fgetc(ls->src);
 		if (!(IS_LETTER(cx) || IS_NUMBER(cx)) || cx == EOF)
