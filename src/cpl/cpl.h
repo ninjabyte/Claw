7368b99566523a82bfc69ec16931fb8f64eadeec
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-08 16:04:26 +0100

    Added permissions

--- src/cpl/cpl.h
+++ src/cpl/cpl.h
@@ -30,13 +30,30 @@ enum Instruction
 	IN_NONE = -1
 };
 
+typedef struct
+{
+	uint8_t use_wifi;
+	// TODO: Add more permissions
+} CplPermissions;
+
+typedef struct
+{
+	char name[15];
+	char author[10];
+	char copyright[15];
+	uint8_t version[3];
+	CplPermissions* permissions;
+} CplHeader;
+
 // compiler state
 typedef struct
 {
 	FILE* dst;	// the destination file
+	CplHeader ch; // header
 } CplState;
 
-void cpl_init(CplState* cs, FILE* fp);
+void cpl_init(CplState* cs, CplHeader* ch, FILE* fp);
+void cpl_write_header(CplState* cs);
 void cpl_write_instr(CplState* cs, int instr);
 void cpl_write_arg8(CplState* cs, uint8_t arg);
 void cpl_write_arg16(CplState* cs, int16_t arg);
