9381bbc6bb68409c327149b1dcb0e926f0beeb98
RobinDeWalvis <voetterrb1a@gmail.com>
2015-11-08 02:03:26 +0100

    this notation is way better :<

--- src/lex/lex.c
+++ src/lex/lex.c
@@ -139,8 +139,8 @@ int lex_nextWord(LexState* ls, char c0)
 	}
 
 	name[0] = cx;
-	uint8_t i = 1;
-	for ( ; i<16; i++)
+	uint8_t i;
+	for (i=1 ; i<16; i++)
 	{
 		cx = fgetc(ls->src);
 		if (!(IS_LETTER(cx) || IS_NUMBER(cx)) || cx == EOF)
