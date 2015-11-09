7d60146904d635c565da8359788e75a685c105eb
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-09 15:43:36 +0100

    Compiler functions now do something

--- src/cpl/cpl.c
+++ src/cpl/cpl.c
@@ -20,15 +20,16 @@ void cpl_write_header(CplState* cs)
 
 void cpl_write_instr(CplState* cs, instr_t instr)
 {
-
+	fputc(instr, cs->dst);
 }
 
 void cpl_write_arg8(CplState* cs, uint8_t arg)
 {
-
+	fputc(arg, cs->dst);
 }
 
 void cpl_write_arg16(CplState* cs, int16_t arg)
 {
-
+	fputc(arg >> 8, cs->dst);
+	fputc(arg & 0xFF, cs->dst);
 }
