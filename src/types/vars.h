57dbc40d60ff4027968ac20233177852865f5367
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-10 17:52:33 +0100

    Added var prototypes

---
+++ src/types/vars.h
@@ -0,0 +1,42 @@
+/*
+ * func.h
+ *
+ *  Created on: 10.11.2015
+ *      Author: Benedikt
+ */
+
+#ifndef TYPES_VARS_H_
+#define TYPES_VARS_H_
+
+typedef uint8_t array_val_t;
+typedef int16_t numeric_val_t;
+
+typedef uint8_t varscope_t;
+typedef uint8_t vartype_t;
+
+enum
+{
+	VARTYPE_NUMBER,
+	VARTYPE_ARRAY
+} VarType;
+
+typedef struct
+{
+	char* name;
+	varscope_t level;
+	uint8_t type;
+} var_t;
+
+typedef struct
+{
+	char* name;
+	vartype_t type;
+} arg_proto_t;
+
+typedef struct
+{
+	char* name;
+	arg_proto_t** prototype;
+} function_t;
+
+#endif /* TYPES_VARS_H_ */
