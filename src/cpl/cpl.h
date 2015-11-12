59596c5094b6b8a7990a4a2ba0527ec893f6e387
Benedikt Müssig <benedikt.muessig@gmail.com>
2015-11-13 00:42:42 +0100

    Added zeros to instruction enum numbers

--- src/cpl/cpl.h
+++ src/cpl/cpl.h
@@ -15,22 +15,22 @@ typedef uint8_t instr_t;
 typedef uint32_t permission_t;
 
 typedef enum {
-	IN_NOP = 0x0,	 /* Do nothing. */
-	IN_ADD = 0x1,	 /* Pops $0 and $1. Pushes $1   $0. */
-	IN_SUB = 0x2,	 /* Pops $0 and $1. Pushes $1 - $0. */
-	IN_MUL = 0x3,	 /* Pops $0 and $1. Pushes $1 * $0. */
-	IN_DIV = 0x4,	 /* Pops $0 and $1. Pushes $1 / $0. This is a signed division. */
-	IN_RICK = 0x5,	 /* nice */
-	IN_LD = 0x6,	 /* Pushes the value of the numeric variable. */
-	IN_LDS = 0x7,	 /* Pops $0. Pushes the numeric variable referenced by $0. */
-	IN_LDA = 0x8,	 /* Pops $0. Pushes the number at index $0 of the array. */
-	IN_LDAS = 0x9,	 /* Pops $0 and $1. Pushes the number at index $1 of the array referenced by $0. */
-	IN_LDC = 0xa,	 /* Pushes the constant. */
-	IN_ST = 0xb,	 /* Pops $0. Stores $0 in the numeric variable. */
-	IN_STS = 0xc,	 /* Pops $0 and $1. Stores $1 in the numeric variable referenced by $0. */
-	IN_STA = 0xd,	 /* Pops $0 and $1. Stores $1 at index $0 of the array. */
-	IN_STAS = 0xe,	 /* Pops $0, $1 and $2. Stores $2 at index $1 of the array referenced by $0. */
-	IN_DEL = 0xf,	 /* Pops $0. */
+	IN_NOP = 0x00,	 /* Do nothing. */
+	IN_ADD = 0x01,	 /* Pops $0 and $1. Pushes $1   $0. */
+	IN_SUB = 0x02,	 /* Pops $0 and $1. Pushes $1 - $0. */
+	IN_MUL = 0x03,	 /* Pops $0 and $1. Pushes $1 * $0. */
+	IN_DIV = 0x04,	 /* Pops $0 and $1. Pushes $1 / $0. This is a signed division. */
+	IN_RICK = 0x05,	 /* nice */
+	IN_LD = 0x06,	 /* Pushes the value of the numeric variable. */
+	IN_LDS = 0x07,	 /* Pops $0. Pushes the numeric variable referenced by $0. */
+	IN_LDA = 0x08,	 /* Pops $0. Pushes the number at index $0 of the array. */
+	IN_LDAS = 0x09,	 /* Pops $0 and $1. Pushes the number at index $1 of the array referenced by $0. */
+	IN_LDC = 0x0a,	 /* Pushes the constant. */
+	IN_ST = 0x0b,	 /* Pops $0. Stores $0 in the numeric variable. */
+	IN_STS = 0x0c,	 /* Pops $0 and $1. Stores $1 in the numeric variable referenced by $0. */
+	IN_STA = 0x0d,	 /* Pops $0 and $1. Stores $1 at index $0 of the array. */
+	IN_STAS = 0x0e,	 /* Pops $0, $1 and $2. Stores $2 at index $1 of the array referenced by $0. */
+	IN_DEL = 0x0f,	 /* Pops $0. */
 	IN_SWP = 0x10,	 /* Pops $0 and $1. Pushes $0 and then $1. */
 	IN_DUP = 0x11,	 /* Pushes $0 to the stack. */
 	IN_VC = 0x12,	 /* Creates a new variable. */
